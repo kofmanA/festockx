f2d84012fd38ed0617ac29d77d34de1e2a518aff
Gor_F <felicia.gorgatchov@gmail.com>
2018-11-22 16:35:34 -0500

    add developers section

--- app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/AboutActivity.java
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/AboutActivity.java
@@ -1,4 +1,8 @@
 package ca.qc.dawsoncollege.stockx.festockx;
 
 public class AboutActivity {
+
+    //single showDescription method --> switch statement
+    //check for which text box value was clicked or which image view was clicked
+    //to display appropriate description
 }
