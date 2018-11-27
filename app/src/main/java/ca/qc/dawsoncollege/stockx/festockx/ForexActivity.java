cbcd05e4e95f975ee211ff212706341f4b6e3f16
Gor_F <felicia.gorgatchov@gmail.com>
2018-11-27 12:15:08 -0800

    create and arrange layout and class, add to manifest

---
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/ForexActivity.java
@@ -0,0 +1,14 @@
+package ca.qc.dawsoncollege.stockx.festockx;
+
+import android.os.Bundle;
+
+public class ForexActivity extends MenuActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forex);
+    }
+
+
+}
