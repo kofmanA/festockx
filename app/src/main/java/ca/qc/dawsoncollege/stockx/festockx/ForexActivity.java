9d567ccb72d8c3dfd402da53c6df4791457873a8
Félicia Gorgatchov <felicia.gorgatchov@dawsoncollege.qc.ca>
2018-11-28 17:04:38 -0500

    display currencies in the spinner to let user choose

--- app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/ForexActivity.java
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/ForexActivity.java
@@ -5,14 +5,19 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -21,16 +26,22 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import javax.net.ssl.HttpsURLConnection;
+
 import static android.content.ContentValues.TAG;
 
 public class ForexActivity extends MenuActivity {
 
-    private JSONObject jobj;
+    private JSONObject json;
+    private String fromItem;
+    private String toItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forex);
+
+        new RetrieveExchangeRates().execute("https://api.exchangeratesapi.io/latest");
     }
 
     //get amount entered -- show error message if button pressed but nothing selected or stuff missing
@@ -39,11 +50,29 @@ public class ForexActivity extends MenuActivity {
     //make appropriate call to api to get conversion information
 
     //calculate
-    public double calculate (double from, double to){
+    public double convert (View view){
+        fromItem = ((Spinner)findViewById(R.id.from)).getSelectedItem().toString();
+        toItem = ((Spinner)findViewById(R.id.to)).getSelectedItem().toString();
+
+        new AsyncTask<String, Void, String>(){
+
+            @Override
+            protected String doInBackground(String... url) {
+                try {
+                    return callUrl(url[0]);
+                } catch (IOException e) {
+                    Log.e(TAG, "exception thrown by download" );
+                    return "Unable to retrieve web page. URL may be invalid.";
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+
+            }
+        };
 
 
-
-        return from;
     }
 
     //display
@@ -51,90 +80,86 @@ public class ForexActivity extends MenuActivity {
 
     private class RetrieveExchangeRates extends AsyncTask<String,Void,String> {
 
-        protected void onPostExecute(String result) {
-            try {
-                Log.d("result: ", result + "");
-                jobj = new JSONObject(result);
-
-                if(jobj.has("rates")){
-                    JSONObject currenciesObject = jobj.getJSONObject("rates");
-                    ArrayList<String> currenciesIterator = currenciesObject.keys();
-                    String[] currencies = new String[currenciesObject.length()];
-
-                    for(String currency : currenciesIterator){
-
-                    }
-
-                    Spinner from = (Spinner) findViewById(R.id.from);
-                    Spinner to = (Spinner) findViewById(R.id.to);
-
-                    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
-                    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-                    spinner.setAdapter(spinnerArrayAdapter);
-
-
-                }
-
-                //Pass retreived ticker info objects into the adapter
-                //tickerInfoList = (ListView) findViewById(R.id.allTickers);
-                //tickerInfoList.setAdapter(new TickerInfoDisplayAdapter(ShowTickerInfoActivity.this, tickersInfo));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-
         @Override
-        protected String doInBackground(String... urls) {
+        protected String doInBackground(String... url) {
             try {
-                return callUrl(urls[0]);
+                return callUrl(url[0]);
             } catch (IOException e) {
                 Log.e(TAG, "exception thrown by download" );
                 return "Unable to retrieve web page. URL may be invalid.";
             }
         }
-    }
 
-    private String downloadUrl(String myUrl) throws IOException {
-        InputStream is = null;
-        HttpURLConnection conn = null;
-        URL url = new URL(myUrl);
-        try {
-            conn = (HttpURLConnection) url.openConnection() ;
-            conn.setReadTimeout(10000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");
-            //Allow input
-            conn.setDoInput(true);
-            conn.connect();
-
-            int response = conn.getResponseCode();
-            if(response != HttpsURLConnection.HTTP_OK){
-                return "Server returned: " + response + " aborting read";
-            }
-            //The request is fine
-            is = conn.getInputStream();
-            return readIt(is);
-        } catch(IOException e) {
-            Log.e(TAG, "IO exception in bg");
-            Log.getStackTraceString(e);
-            throw e;
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (IOException ignore) {
+        protected void onPostExecute(String result) {
+            try {
+                if(result == null){
+                    return;
                 }
-                if (conn != null)
-                    try {
-                        conn.disconnect();
-                    } catch (IllegalStateException ignore ) {
+                Log.d("result: ", result + "");
+                json = new JSONObject(result);
+
+                if(json.has("rates")){
+                    JSONObject currenciesObject = json.getJSONObject("rates");
+                    Iterator<String> currenciesIterator = currenciesObject.keys();
+                    List<String> currencies = new ArrayList<>();
+
+                    while(currenciesIterator.hasNext()){
+                        currencies.add(currenciesIterator.next());
                     }
+
+                    Spinner from = (Spinner) findViewById(R.id.from);
+                    Spinner to = (Spinner) findViewById(R.id.to);
+
+                    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(ForexActivity.this, android.R.layout.simple_spinner_item, currencies);
+                    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                    from.setAdapter(spinnerArrayAdapter);
+                    to.setAdapter(spinnerArrayAdapter);
+
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+
+    }
+
+
+    private String callUrl(String urlParam) throws IOException {
+        URL url = new URL(urlParam); //HTTP/1.1
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setReadTimeout(5000);
+        connection.setConnectTimeout(5000);
+        connection.setRequestMethod("GET");
+        connection.setDoInput(true);
+        connection.connect  ();
+
+        int response = connection.getResponseCode();
+        if (response == HttpURLConnection.HTTP_OK) {
+            InputStream instream = connection.getInputStream();
+
+            return readIt(instream);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle(R.string.error)
+                    .setMessage(R.string.errmsg)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .show();
+        }
+
+        connection.disconnect();
+
+        return null;
+
     }
 
     public String readIt(InputStream is) throws IOException,UnsupportedEncodingException {
+        final int BUFFER = 1024;
         int bytesRead;
         int totalRead = 0;
         byte[] buffer = new byte[BUFFER];
@@ -152,41 +177,6 @@ public class ForexActivity extends MenuActivity {
         }
         writer.flush();
         return new String(byteArrayOutputStream.toString());
-    }
-
-
-    private void callUrl() throws IOException {
-        URL url = new URL("https://api.exchangeratesapi.io/latest"); //HTTP/1.1
-        HttpURLConnection connection  = (HttpURLConnection) url.openConnection();
-
-        connection.setReadTimeout(5000);
-        connection.setConnectTimeout(5000);
-        connection.setRequestMethod("GET");
-
-        connection.connect();
-
-        int response = connection.getResponseCode();
-        if(response == HttpURLConnection.HTTP_OK){
-            InputStream instream = connection.getInputStream();
-            //read
-
-
-            instream.close();
-        }
-        else{
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-            builder.setTitle(R.string.error)
-                    .setMessage(R.string.errmsg)
-                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    })
-                    .show();
-        }
-
-        connection.disconnect();
-
     }
 
 }
