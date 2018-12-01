008193f09053ae76924f9ab9aca66007536b8070
Alex Kofman <akofman123@gmail.com>
2018-12-01 13:30:36 -0500

    Rename stock activity to settings activity

--- app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/SettingsActivity.java
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/SettingsActivity.java
@@ -1,14 +1,13 @@
 package ca.qc.dawsoncollege.stockx.festockx;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StockActivity extends Activity {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stock);
+        setContentView(R.layout.activity_settings);
     }
 }
