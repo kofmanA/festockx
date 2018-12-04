e63506bcaafbff1f28c087a4924cced5794f354e
Alex Kofman <akofman123@gmail.com>
2018-12-04 17:57:12 -0500

    Try to add loading into SharePreferences

--- app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/StockTicker/StockNumberSelectActivity.java
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/StockTicker/StockNumberSelectActivity.java
@@ -35,7 +35,19 @@ public class StockNumberSelectActivity extends Activity {
         Integer[] numTickers = new Integer[]{1, 2, 3, 4, 5};
 
         numberSpinner = (Spinner) findViewById(R.id.stock_number);
-
+        //If there's a saved instance state, load the information from it into all of the textboxes
+        if(savedInstanceState != null){
+            list = (ListView) findViewById(R.id.tickerBoxes);
+            int number = (int) numberSpinner.getSelectedItem();
+            for (int i = 0; i < number; i++) {
+                Log.d("childFound","hi");
+                String indexKey = "box" + i + "";
+                LinearLayout child = (LinearLayout) list.getChildAt(i);
+                EditText content = (EditText) child.getChildAt(0);
+                String value = savedInstanceState.getString(indexKey);
+                content.setText(value);
+            }
+        }
         ArrayAdapter<Integer> dataAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, numTickers);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         numberSpinner.setAdapter(dataAdapter);
@@ -75,7 +87,6 @@ public class StockNumberSelectActivity extends Activity {
      */
     public List<String> getListOfTickers(){
         List<String> tickers = new ArrayList<String>();
-
         for (int i = 0; i < list.getChildCount(); i++) {
             if (list.getChildAt(i) instanceof LinearLayout) {
                 LinearLayout child = (LinearLayout) list.getChildAt(i);
@@ -88,6 +99,33 @@ public class StockNumberSelectActivity extends Activity {
         return tickers;
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ListView myList = (ListView) findViewById(R.id.tickerBoxes);
+        for (int i = 0; i < myList.getChildCount(); i++) {
+            String indexKey = "box" + i + "";
+            if (myList.getChildAt(i) instanceof LinearLayout) {
+                LinearLayout child = (LinearLayout) myList.getChildAt(i);
+                EditText content = (EditText) child.getChildAt(0);
+                outState.putString(indexKey,content.getText().toString());
+            }
+        }
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ListView myList = (ListView) findViewById(R.id.tickerBoxes);
+        for (int i = 0; i < myList.getChildCount(); i++) {
+            String indexKey = "box" + i + "";
+            if (myList.getChildAt(i) instanceof LinearLayout) {
+                String value = savedInstanceState.getString(indexKey);
+                LinearLayout child = (LinearLayout) myList.getChildAt(i);
+                EditText content = (EditText) child.getChildAt(0);
+                content.setText(value);
+            }
+        }
+    }
 
 }
