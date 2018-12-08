00902a4e2109a14ec6e7ec31a8dab3b560120054
Alex Kofman <akofman123@gmail.com>
2018-12-08 14:22:51 -0500

    Try to do login along with API call

--- app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/StockTicker/TickerInfoDisplayAdapter.java
+++ app/src/main/java/ca/qc/dawsoncollege/stockx/festockx/StockTicker/TickerInfoDisplayAdapter.java
@@ -20,6 +20,7 @@ import android.support.v7.app.*;
 import android.support.v4.*;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -27,9 +28,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -40,6 +45,8 @@ import ca.qc.dawsoncollege.stockx.festockx.SQLite.NoteItemActivity;
 import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDisplayAdapter.Holder> {
+    private String moneyLeft;
+    private String JWTToken;
     private String stockName;
     private String numStocksToBuy;
     private JSONObject jsonObj;
@@ -49,14 +56,15 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
     private int BUFFER = 1024;
 
 
-    public TickerInfoDisplayAdapter(Context context, List<TickerStock> tickers){
+    public TickerInfoDisplayAdapter(Context context, List<TickerStock> tickers) {
         this.context = context;
         this.tickers = tickers;
     }
 
+
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_ticker_info,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_ticker_info, parent, false);
         Holder holder = new Holder(view);
 
         view.setOnLongClickListener(v -> {
@@ -73,15 +81,39 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
                     .setCancelable(false)
                     .setPositiveButton(R.string.buyStock,
                             new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //GET USER INFO
+                                    JSONObject authenticationJSON = new JSONObject();
+                                    try {
+                                        authenticationJSON.put("email", "asd@asd.asd");
+                                        authenticationJSON.put("password", "asdasdasd");
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                    final HashMap<String, String> authData = new HashMap<>();
+                                    authData.put("url", "http://stockxportfolio.herokuapp.com/api/auth/login");
+                                    authData.put("method", "POST");
+                                    authData.put("data", authenticationJSON.toString());
+
+                                    ConnectivityManager connMgr = (ConnectivityManager) holder.getContext().getSystemService(holder.getContext().CONNECTIVITY_SERVICE);
+                                    NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
+
+                                    //LOGIN, retreive JWT Token
+                                    if (netInfo != null && netInfo.isConnected()) {
+                                        new Request().execute();
+                                    }
+
                                     //Get number of stocks to buy and the name of the stock they want to purchase
                                     numStocksToBuy = userInput.getText().toString();
                                     stockName = holder.symbolName.getText().toString();
+
+                                    //Send Api request to our own API with numStocks and stockName
+                                    getCashLeft(holder);
                                 }
                             })
                     .setNegativeButton(R.string.cancel,
                             new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
+                                public void onClick(DialogInterface dialog, int id) {
                                     dialog.cancel();
                                 }
                             });
@@ -95,7 +127,7 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
     @Override
     public void onBindViewHolder(Holder holder, final int position) {
         TickerStock tickerStats = tickers.get(position);
-        Log.d(TAG,tickerStats.getTickerName());
+        Log.d(TAG, tickerStats.getTickerName());
         holder.symbolName.setText(tickerStats.getTickerName());
         holder.companyName.setText(tickerStats.getCompanyName());
         holder.currentPrice.setText(tickerStats.getPrice());
@@ -111,10 +143,7 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
     }
 
 
-
-
-
-    public class Holder extends RecyclerView.ViewHolder {
+    class Holder extends RecyclerView.ViewHolder {
         public Context context;
         TextView symbolName;
         TextView companyName;
@@ -136,12 +165,12 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
             changePct = (TextView) itemView.findViewById(R.id.changePercentDisplay);
         }
 
-        public Context getContext(){
+        public Context getContext() {
             return this.context;
         }
     }
 
-    public void getCashLeft(Holder holder){
+    public void getCashLeft(Holder holder) {
         /**
          * TODO:
          * Create second Async Class to log user in and receive bearer token
@@ -152,36 +181,32 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
         //Get token of logged in user from settings
         String loginUrl = "";
 
-
-        String bearerToken = "bk3gi7ZJHWLyeG4QhI2ruwRqLsZCLhNCIpn0qYivknYwL5I3p8emaJD5ABwo";
-
         //change to heroku URL for later
-        String url = "https://ass3.test/api/api/buy?api_token=" + bearerToken + "&quantity=" + numStocksToBuy + "&name=" + stockName ;
+        String url = "http://stockxportfolio.herokuapp.com/api/api/buy?quantity=" + numStocksToBuy + "&name=" + stockName;
 
         ConnectivityManager connMgr = (ConnectivityManager) holder.getContext().getSystemService(holder.getContext().CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
-        if(netInfo != null && netInfo.isConnected()){
-           new BuyStock().execute(url);
+        if (netInfo != null && netInfo.isConnected()) {
+            new BuyStock().execute(url);
         }
     }
 
 
-    private class BuyStock extends AsyncTask<String,Void,String> {
+    class BuyStock extends AsyncTask<String, Void, String> {
 
         protected void onPostExecute(String result) {
             try {
                 Log.d("result: ", result + "");
                 jsonObj = new JSONObject(result);
-               // Intent i = new Intent(this, PortolioActivity.class);
-                if(jsonObj.has("cashleft")){
-                    String moneyLeft = jsonObj.getString("cashleft");
-                //   i.putExtra("cashleft",moneyLeft);
-                     popUpMoneyDialog(moneyLeft);
-                }
-                else if(jsonObj.has("error")){
+                // Intent i = new Intent(this, PortolioActivity.class);
+                if (jsonObj.has("cashleft")) {
+                    moneyLeft = jsonObj.getString("cashleft");
+                    //   i.putExtra("cashleft",moneyLeft);
+                    popUpMoneyDialog(moneyLeft);
+                } else if (jsonObj.has("error")) {
                     String errorMsg = jsonObj.getString("error");
-                   // i.putExtra("error",errorMsg);
-                   // startActivity(i);
+                    // i.putExtra("error",errorMsg);
+                    // startActivity(i);
 
                 }
             } catch (Exception e) {
@@ -195,16 +220,16 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
             try {
                 return downloadUrl(urls[0]);
             } catch (IOException e) {
-                Log.e(TAG, "exception thrown by download" );
+                Log.e(TAG, "exception thrown by download");
                 return "Unable to retrieve web page. URL may be invalid.";
             }
         }
 
-        private void  popUpMoneyDialog(String moneyLeft){
+        private void popUpMoneyDialog(String moneyLeft) {
             DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    switch (which){
+                    switch (which) {
                         case DialogInterface.BUTTON_POSITIVE:
                             //launch activity
                             break;
@@ -216,7 +241,7 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
                 }
             };
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setTitle(R.string.moneyLeftDialogTitle).setMessage(R.string.moneyLeftDialogMessage+moneyLeft+"$."+R.string.moneyLeftDialogGoTo).setPositiveButton(R.string.Yes, dialogClickListener)
+            builder.setTitle(R.string.moneyLeftDialogTitle).setMessage(R.string.moneyLeftDialogMessage + moneyLeft + "$." + R.string.moneyLeftDialogGoTo).setPositiveButton(R.string.Yes, dialogClickListener)
                     .setNegativeButton(R.string.No, dialogClickListener).show();
 
         }
@@ -228,23 +253,23 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
         HttpURLConnection conn = null;
         URL url = new URL(myUrl);
         try {
-            conn = (HttpURLConnection) url.openConnection() ;
+            conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000);
             conn.setConnectTimeout(15000);
             conn.setRequestMethod("GET");
             //Allow input
             conn.setDoInput(true);
-            conn.setRequestProperty("Authorization:","Bearer ");
+            conn.setRequestProperty("Authorization:", "Bearer ");
             conn.connect();
 
             int response = conn.getResponseCode();
-            if(response != HttpsURLConnection.HTTP_OK){
+            if (response != HttpsURLConnection.HTTP_OK) {
                 return "Server returned: " + response + " aborting read";
             }
             //The request is fine
             is = conn.getInputStream();
             return readIt(is);
-        } catch(IOException e) {
+        } catch (IOException e) {
             Log.e(TAG, "IO exception in bg");
             Log.getStackTraceString(e);
             throw e;
@@ -257,7 +282,7 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
                 if (conn != null)
                     try {
                         conn.disconnect();
-                    } catch (IllegalStateException ignore ) {
+                    } catch (IllegalStateException ignore) {
                     }
             }
         }
@@ -266,25 +291,25 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
     private String getBearerToken(String email, String pword) throws IOException {
         InputStream is = null;
         HttpURLConnection conn = null;
-        URL url = new URL(email+pword);
+        URL url = new URL(email + pword);
         try {
-            conn = (HttpURLConnection) url.openConnection() ;
+            conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000);
             conn.setConnectTimeout(15000);
             conn.setRequestMethod("GET");
             //Allow input
             conn.setDoInput(true);
-            conn.setRequestProperty("Authorization:","Bearer ");
+            conn.setRequestProperty("Authorization:", "Bearer ");
             conn.connect();
 
             int response = conn.getResponseCode();
-            if(response != HttpsURLConnection.HTTP_OK){
+            if (response != HttpsURLConnection.HTTP_OK) {
                 return "Server returned: " + response + " aborting read";
             }
             //The request is fine
             is = conn.getInputStream();
             return readIt(is);
-        } catch(IOException e) {
+        } catch (IOException e) {
             Log.e(TAG, "IO exception in bg");
             Log.getStackTraceString(e);
             throw e;
@@ -297,13 +322,13 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
                 if (conn != null)
                     try {
                         conn.disconnect();
-                    } catch (IllegalStateException ignore ) {
+                    } catch (IllegalStateException ignore) {
                     }
             }
         }
     }
 
-    public String readIt(InputStream is) throws IOException,UnsupportedEncodingException {
+    public String readIt(InputStream is) throws IOException, UnsupportedEncodingException {
         int bytesRead;
         int totalRead = 0;
         byte[] buffer = new byte[BUFFER];
@@ -321,5 +346,79 @@ public class TickerInfoDisplayAdapter extends RecyclerView.Adapter<TickerInfoDis
         }
         writer.flush();
         return new String(byteArrayOutputStream.toString());
+    }
+
+    //This class should take the email and password and return the token to class's JWTToken field
+    class Request extends AsyncTask<HashMap<String, String>, Void, String> {
+
+        //Cache the JWT Token received from the login
+        @Override
+        protected void onPostExecute(String result){
+            try {
+                Log.d("TEst", "onPostExecute: " + result);
+                JSONObject json = new JSONObject(result);
+                JWTToken = json.getString("access_token");
+                Log.d("TOKEN", "onPostExecute: " + JWTToken);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            };
+        }
+
+
+        //Send the request to the login API to get the JWT token
+        @Override
+        protected String doInBackground(HashMap<String, String>... data) {
+            HttpURLConnection connection = null;
+            InputStream instream = null;
+            try {
+                URL url = new URL(data[0].get("url")); //HTTP/1.1
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(false);
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(10000);
+                Log.d("METHOD", "doInBackground: " + data[0].get("method"));
+                connection.setRequestMethod(data[0].get("method"));
+                connection.setRequestProperty("Content-Type", "application/json");
+
+                connection.setRequestProperty("Authorization", "Bearer " + JWTToken);
+                Log.d("DATA", "doInBackground: " + data[0].get("data"));
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+                writer.write(data[0].get("data"));
+                writer.flush();
+
+                connection.connect();
+
+                int response = connection.getResponseCode();
+                Log.d("RESPONSE", "doInBackground: " + response);
+                if (response == HttpURLConnection.HTTP_OK) {
+                    instream = connection.getInputStream();
+
+                    return readIt(instream);
+                } else {
+                    this.cancel(true);
+                    return "";
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (instream != null && connection != null) {
+                        instream.close();
+                        connection.disconnect();
+                    }
+                } catch (Exception e) {
+                    Log.e("Error", "problem closing input stream or html connection");
+                }
+            }
+            Log.d("NULL", "doInBackground: REACHED NULL");
+            return "";
+        }
     }
 }
